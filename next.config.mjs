0da8e1ece407fe78e85f413dd75165931c2e85c2
mediaprophet <34872169+mediaprophet@users.noreply.github.com>
2025-07-14 07:41:13 +1000

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import withNextra from 'nextra';
+
+export default withNextra({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.mjs',
+  standalone: true
+})({
   output: 'export',
   basePath: '/webizen-app-docs',
   trailingSlash: true,
   images: {
     unoptimized: true
   }
-};
-
-export default nextConfig;
+});
