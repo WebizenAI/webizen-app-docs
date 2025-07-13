260db0cbc0bbb49d52e447bceb36c85b7d9f356c
mediaprophet <34872169+mediaprophet@users.noreply.github.com>
2025-07-14 05:49:59 +1000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,2 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-
-export default function Index() {
-  const router = useRouter();
-  useEffect(() => {
-    router.replace('/webizen-app-docs/docs');
-  }, [router]);
-  return null;
-}
+// This file intentionally left blank for Nextra to render docs at the root.
+export { default } from '../docs/index.mdx';
