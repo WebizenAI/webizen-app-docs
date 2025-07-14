134b4350fcbb5687b248420e2079e856c2d5388c
mediaprophet <34872169+mediaprophet@users.noreply.github.com>
2025-07-14 03:39:36 +0000

    simplify

--- nextra.config.mjs
+++
@@ -1,5 +0,0 @@
-import { withDocs } from 'nextra-theme-docs';
-
-export default withDocs({
-  themeConfig: './theme.config.jsx'
-});
--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  content: [
-    './content/**/*.{js,jsx,md,mdx}',
-    './components/**/*.{js,jsx}',
-    './theme.config.mjs'
-  ],
-  theme: { extend: {} },
-  plugins: []
-};
